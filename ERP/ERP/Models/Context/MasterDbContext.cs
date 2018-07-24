8c78ad2e6d7bf110bf1a1647508862d9f3122c1d
Ronit Roy <get.ronit.roy@gmail.com>
2018-07-24 22:35:25 +0530

    Add user page added

---
+++ ERP/ERP/Models/Context/MasterDbContext.cs
@@ -0,0 +1,20 @@
+﻿using ERP.Models.DataTemplates.Master;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ERP.Models.Context
+{
+    public class MasterDbContext:DbContext
+    {
+        public MasterDbContext()
+        {
+            this.Database.Connection.ConnectionString = @"Data Source=(LocalDB)\v11.0;AttachDbFilename=C:\Users\RONIT\Documents\ERP.mdf;Integrated Security=True;Connect Timeout=30";
+        }
+
+        public DbSet<user> users { get; set; }
+        public DbSet<group> groups { get; set; }
+    }
+}
